2ef16d7e62dd061a5ff64c975df13520a7a15cbc
kaprilis <kincky@yeah.net>
2014-02-21 17:08:19 +0800

    ++

---
+++ app/src/main/java/com/whaim/alarmnow/app/Alarm.java
@@ -0,0 +1,8 @@
+package com.whaim.alarmnow.app;
+
+/**
+ * Created by whaim on 14-2-21.
+ */
+public class Alarm {
+    private
+}
---
+++ app/src/main/java/com/whaim/alarmnow/app/AlarmDBM.java
@@ -0,0 +1,7 @@
+package com.whaim.alarmnow.app;
+
+/**Operate alarm database
+ * Created by whaim on 14-2-21.
+ */
+public class AlarmDBM {
+}
